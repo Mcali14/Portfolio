e4ece7a338a06570a28a41642a3b4f23fa8593fa
Alistair VIGLIERI <alizzio22@gmail.com>
2019-04-18 12:28:43 +0200

    modif fichier cv

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <title>Portfolio VIGLIERI Alistair</title>
     <link rel="stylesheet" href="assets/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <script src="assets/js/script.js" type="text/javascript"></script>
+    <script src="assets/js/script.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
 
 </head>
@@ -15,10 +15,12 @@
       <div class="container">
         <a href="index.html" class="logo">Mon portfolio</a>
         <nav class="menu">
-          <a href="#about">A propos </a>
-          <a href="#portfolio">Portfolio</a>
-          <a href="#Contact">Contact</a>
-          <a href="#Infos">Infos perso</a>
+          <nav class="menu">
+            <a href="#about">A propos </a>
+            <a href="#portfolio">Portfolio</a>
+            <a href="#Contact">Contact</a>
+            <a href="#Infos">Infos perso</a>
+          </nav>
         </nav>
       </div>
   </header>
@@ -29,7 +31,7 @@
         <div class="inner-banner">
           <h1>Bienvenue sur mon portfolio</h1>
           <button type="button" class="btn btn-primary">Contactez-moi&nbsp;!</button>
-          <a href="cv/ALISTAIR VIGLIERI.pdf" download="ALISTAIR VIGLIERI.pdf"><button type="button" class="btn btn-danger">Télécharger mon cv !</button></a>
+          <a href="cv/ALISTAIR_VIGLIERI.pdf" download="ALISTAIR_VIGLIERI.pdf"><button type="button" class="btn btn-danger">Télécharger mon cv !</button></a>
         </div>
     </section>
     <section class="container-fluid about">
@@ -46,21 +48,21 @@
                 <li>Améliorer le positionnement d'un site web </li>
                 <li>Administrer et gérer un site sous Wordpress </li>
                 <li>Communiquer et prospecter via des outils en ligne </li>
-                <li>Mettre en place des campagnes publicitaires</li>
+                <li>Mettre en place des campagn_es publicitaires</li>
                 <li>Communiquer en ligne</li>
               </ul>
             </article>
 
             <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
               <h2>Savoir</h2>
-
-              <li>Techniques d’animations web </li>
-              <li>Référencement web et Référencement naturel (SEO) </li>
-              <li>Rédaction d'un contenu web</li>
-              <li>Web marketing et Marketing des réseaux sociaux</li>
-              <li>Community management </li>
-              <li>Gérer un site web</li>
-
+              <ul>
+                <li>Techniques d’animations web </li>
+                <li>Référencement web et Référencement naturel (SEO) </li>
+                <li>Rédaction d'un contenu web</li>
+                <li>Web marketing et Marketing des réseaux sociaux</li>
+                <li>Community management </li>
+                <li>Gérer un site web</li>
+              </ul>
             </article>
 
             <article class="col-md-4 col-lg-4 col-xs-12 col-sm-12">
@@ -83,24 +85,34 @@
         <div class="container">
           <h2 id="portfolio">Mon portfolio</h2>
           <hr class="separateur">
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test1" href=>
-            </article>
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test2">
-            </article>
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test3">
-            </article>
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test4">
-            </article>
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test3">
-            </article>
-            <article class="col-md-3 col-lg-3 col-xs-12 col-sm-12 item-folio">
-              <img src="img/e-commerce.jpg" alt="Test3">
-            </article>
+
+            <ul>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+              <li>
+                  <a href="#"><img src="img/e-commerce.jpg" alt="Site e-commerce"/></a>
+              </li>
+            </ul>
+
         </div>
     </section>
 <?php
@@ -109,10 +121,87 @@ if(!isset($company)) $company ='';
 if(!isset($mail)) $mail ='';
 if(!isset($tel)) $tel ='';
  ?>
+
+ <?php
+
+ try {
+     $pdo = new PDO('mysql:host=localhost;dbname=portfolio',
+         'root',
+         '',
+         array(
+             PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
+         ));
+ }
+ catch (PDOException $e) {
+     echo "Erreur de connexion : " . $e->getMessage();
+ }
+
+ if (isset($_POST['maurice'])) {
+     $nom = isset($_POST['name']) ? $_POST['name'] : "";
+     $company = isset($_POST['company']) ? $_POST['company'] : "";
+     $mail = isset($_POST['mail']) ? $_POST['mail'] : "";
+     $tel = isset($_POST['phone']) ? $_POST['phone'] : "";
+     $erreurs = array();
+
+      if (!(mb_strlen($nom) >= 2 && ctype_alpha($nom)))
+     array_push($erreurs, 'Veuillez saisir votre nom');
+     if (!(mb_strlen($company) >= 2 && ctype_alpha($company)))
+     array_push($erreurs, 'Veuillez saisir votre société');
+     if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+     array_push($erreurs, "Veuillez saisir une adresse mail valide.");
+     if (!(mb_strlen($nom) >= 2))
+    array_push($erreurs, 'Veuillez saisir votre message');
+
+    if (count($erreurs) > 0) {
+        $message = "<ul>";
+        $i = 0;
+        while ($i < count($erreurs)) {
+            $message .= "<li>" . $erreurs[$i] . "</li>";
+            $i++;
+        }
+        $message .= "</ul>";
+        echo $message;
+      }
+
+       else {
+         $sql = "SELECT COUNT(*) FROM t_contact WHERE mail='". $mail . "'";
+         $nombreOccurences = $pdo->query($sql)->fetchColumn();
+         if($nombreOccurences == 0) {
+           $sql ="INSERT INTO t_contact (nom, société, mail, num, message)
+           VALUES ('" . $nom ."', '" . $company ."', '" . $mail ."', '" . $tel ."', '" . $message ."')";
+           $query = $pdo->prepare($sql);
+           $query->bindvalue('nom', $nom, PDO::PARAM_STR);
+           $query->bindvalue('société', $company, PDO::PARAM_STR);
+           $query->bindvalue('mail', $mail, PDO::PARAM_STR);
+           $query->bindvalue('num', $tel, PDO::PARAM_STR);
+           $query->bindvalue('message', $message, PDO::PARAM_STR);
+         }
+         else {
+           echo "erreur dans l'insertion";
+         }
+
+       }
+     }
+
+     else {
+       $message = "Votre message a bien été envoyé";
+     }
+
+     ?>
     <section class="get-in-touch">
-      <h1 id="Contact"class="title">Get in touch</h1>
+      <?php
+        if(isset($message) && $message != ""){
+        echo $resultat;}
+        else {
+          echo "erreur dans l'envoi du message";
+        }
+       ?>
+
+      <h1 id="Contact" class="title">Get in touch</h1>
       <hr class="separateur">
-    <form class="contact-form row" action="contact.php" method="post" onsubmit="validerFrm();">
+    <form class="contact-form row" action="contact.php" method="post" onsubmit="validerFrm()">
         <div class="form-field col-lg-6">
             <input id="name" class="input-text js-input" type="text" name="name" value="<?= $nom ?>">
             <label class="label" for="name">Nom</label>
@@ -130,15 +219,16 @@ if(!isset($tel)) $tel ='';
           <label class="label" for="phone">Téléphone</label>
         </div>
         <div class="form-field col-lg-12">
-          <label class="label "for="message">Message</label>
+          <label class="label " for="message">Message</label>
           <textarea id="message" class="input-text js-input" name="user_message"></textarea>
         </div>
         <div class="form-field col-lg-12">
           <input class="submit-btn" type="submit" value="submit">
         </div>
-        <input type="hidden " name="maurice">
+        <input type="hidden" name="maurice">
     </form>
   </section>
+
   <footer id="dk-footer" class="dk-footer">
         <div class="container">
             <div class="row">
